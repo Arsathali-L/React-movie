96e9e576a1e6ee186f81765d0790fe7d10176e6d
Arsathali-L <arsathliakath092003@gmail.com>
2024-10-20 21:30:00 +0530

    movie

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+// import Logo from "/favicon.png";
+
+function Navbar() {
+	return (
+		<nav className="navbar bg-dark text-white" data-bs-theme="dark">
+			<div className="container-fluid">
+				<a className="navbar-brand fw-bold" href="#">
+					
+					Search Movie
+				</a>
+			</div>
+		</nav>
+	);
+}
+
+export default Navbar;
